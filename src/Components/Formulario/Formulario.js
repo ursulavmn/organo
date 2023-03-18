9c1db4a9b3ea0ef0b90c9ffd2638d7bbdca0a05f
ursula <ursulavmn@gmail.com>
2023-03-18 18:44:20 -0300

    formulario

--- src/Components/Formulario/Formulario.js
+++ src/Components/Formulario/Formulario.js
@@ -7,6 +7,7 @@ const Formulario = () => {
         <section className="formulario" >
             <form>
                 <h2>Preencha os dados para criar o card do colaborador </h2>
+                <h2>novas atualizaçoes</h2>
             <CampoTexto label="Nome:" placeholder="Digite seu Nome "/>
             <CampoTexto label="Cargo:" placeholder="Digite seu Cargo"/>
             <CampoTexto label="Imagem:" placeholder="Digite o Endereço da Imagem"/>
