e92fbc8e8efbb816c387d7f8caa27c4c656f706a
ursula <ursulavmn@gmail.com>
2023-03-19 22:40:01 -0300

    finalizandoo projeto

--- src/Components/Formulario/Formulario.js
+++ src/Components/Formulario/Formulario.js
@@ -1,20 +1,67 @@
-import './Formulario.css'
+import { useState } from 'react'
+import Botao from '../Botao/Botao'
 import CampoTexto from '../CampoTexto/CampoTexto'
+import ListaSuspensa from '../ListaSuspensa/ListaSuspensa'
+import './Formulario.css'
 
+const Formulario = (props) => {
 
-const Formulario = () => {
+    const [nome, setNome] = useState('')
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [time, setTime] = useState('')
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        props.aoColaboradorCadastrado({
+            nome,
+            cargo,
+            imagem,
+            time
+        })
+        setNome('')
+        setCargo('')
+        setImagem('')
+        setTime('')
+    }
+
     return (
-        <section className="formulario" >
-            <form>
-                <h2>Preencha os dados para criar o card do colaborador </h2>
-                <h2>novas atualizaçoes</h2>
-            <CampoTexto label="Nome:" placeholder="Digite seu Nome "/>
-            <CampoTexto label="Cargo:" placeholder="Digite seu Cargo"/>
-            <CampoTexto label="Imagem:" placeholder="Digite o Endereço da Imagem"/>
+        <section className="formulario">
+            <form onSubmit={aoSalvar}>
+                <h2>Preencha os dados para criar o card do colaborador</h2>
+                <CampoTexto 
+                    obrigatorio={true}
+                    label="Nome"
+                    placeholder="Digite seu nome" 
+                    valor={nome}
+                    aoAlterado={valor => setNome(valor)}
+                />
+                <CampoTexto
+                    obrigatorio={true}
+                    label="Cargo"
+                    placeholder="Digite seu cargo" 
+                    valor={cargo}
+                    aoAlterado={valor => setCargo(valor)}
+                />
+                <CampoTexto
+                    label="Imagem"
+                    placeholder="Digite o endereço da imagem" 
+                    valor={imagem}
+                    aoAlterado={valor => setImagem(valor)}
+                />
+                <ListaSuspensa
+                    obrigatorio={true}
+                    label="Time" 
+                    itens={props.times}
+                    valor={time}
+                    aoAlterado={valor => setTime(valor)}
+                />
+                <Botao>
+                    Criar Card
+                </Botao>
             </form>
         </section>
     )
-
 }
 
 export default Formulario
---
+++ src/Components/PrimeiroO/Primeiro.js
@@ -0,0 +1,10 @@
+import './Primeiro.css'
+
+export const Primeiro = () => {
+    // JSX
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
+        </header>
+    );
+};
